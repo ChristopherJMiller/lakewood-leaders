e85ccccdf3354c03323491bcf7e34adf588dbf8d
Christopher Miller <admin@mctherealm.net>
2016-07-04 22:45:46 -0400

    Added events list and time support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def current_user
     session[:user_id] && User.find_by_id(session[:user_id])
   end
+
+  def adjust_time_and_format(time, format = :long)
+    time.in_time_zone('EST').to_formatted_s(format)
+  end
 end
