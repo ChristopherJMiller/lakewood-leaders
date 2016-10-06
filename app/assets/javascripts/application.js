51955092cede2bd3ffd2c4d9d7735f0fce5d580e
Christopher Miller <admin@mctherealm.net>
2016-10-05 22:57:05 -0400

    Added content to dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require tether
 //= require bootstrap-sprockets
-//= require Chart
+//= require Chart.bundle
+//= require chartkick
 //= require navbar
 //= require turbolinks
