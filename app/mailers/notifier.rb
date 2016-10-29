e65c1d1878351d6b327997b66519f596b91923d3
Christopher Miller <admin@mctherealm.net>
2016-10-29 12:42:54 -0400

    Fixed offenses in mailers

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,24 +1,26 @@
+# = Notifier
+# Application Mail for sending emails to users
 class Notifier < ApplicationMailer
-    default from: 'lakewoodleaders@mctherealm.net'
+  default from: 'lakewoodleaders@mctherealm.net'
 
-    def verify_email(user)
-      @user = user
-      mail to: user.email, subject: 'Lakewood Leaders Email Verification'
-    end
+  def verify_email(user)
+    @user = user
+    mail to: user.email, subject: 'Lakewood Leaders Email Verification'
+  end
 
-    def verify_parent_email(user)
-      @user = user
-      mail to: user.parent_email, subject: 'Lakewood Leaders Parent Email Verification'
-    end
+  def verify_parent_email(user)
+    @user = user
+    mail to: user.parent_email, subject: 'Lakewood Leaders Parent Email Verification'
+  end
 
-    def announce(announcement)
-      @announcement = announcement
-      headers['List-Unsubscribe'] = "<https://leaders.lrhsclubs.com/unsubscribe/>"
-      User.connection.select_values(User.select("parent_email").where("parent_verified": true).where.not(parent_email: nil).to_sql) do |parent|
-        mail to: parent, subject: "Lakewood Leaders: #{@announcement.title}"
-      end
-      User.connection.select_values(User.select("email").where(verified: true).to_sql).each do |user|
-        mail to: user, subject: "Lakewood Leaders: #{@announcement.title}"
-      end
+  def announce(announcement)
+    @announcement = announcement
+    headers['List-Unsubscribe'] = '<https://leaders.lrhsclubs.com/unsubscribe/>'
+    User.connection.select_values(User.select('parent_email').where(parent_verified: true).where.not(parent_email: nil).to_sql) do |parent|
+      mail to: parent, subject: "Lakewood Leaders: #{@announcement.title}"
     end
+    User.connection.select_values(User.select('email').where(verified: true).to_sql).each do |user|
+      mail to: user, subject: "Lakewood Leaders: #{@announcement.title}"
+    end
+  end
 end
